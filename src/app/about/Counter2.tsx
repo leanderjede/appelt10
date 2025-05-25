4e5f9f66c596bbafb247c52ffe7c2a7236426b12
leanderjede <info@jede-website.de>
2025-05-25 12:01:56 +0200

    Erster Commit

--- src/app/about/Counter2.tsx
+++ src/app/about/Counter2.tsx
@@ -109,7 +109,7 @@ const CounterSection = () => {
               <div className="w-full lg:justify-start justify-center items-center sm:gap-10 gap-5 inline-flex">
                 <div className="flex-col justify-start items-start inline-flex">
                   <h3 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
-                 8+
+                 15+
                   </h3>
                   <h6 className="text-gray-500 text-base font-normal leading-relaxed">
                  Jahre Erfahrung
@@ -117,7 +117,7 @@ const CounterSection = () => {
                 </div>
                 <div className="flex-col justify-start items-start inline-flex">
                   <h4 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
-                 270+
+                 120+
                   </h4>
                   <h6 className="text-gray-500 text-base font-normal leading-relaxed">
                   Erfolgreiche Projekte
@@ -125,7 +125,7 @@ const CounterSection = () => {
                 </div>
                 <div className="flex-col justify-start items-start inline-flex">
                   <h4 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
-                   117+
+                   120+
                   </h4>
                   <h6 className="text-gray-500 text-base font-normal leading-relaxed">
                   Glückliche Kunden
